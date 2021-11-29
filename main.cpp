7e92d68017b9c2e34229e669e62dedcf50ca2c43
Walter Tang <waltert3@illinois.edu>
2021-11-29 17:06:36 -0600

    need to test data reading

--- main.cpp
+++ main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <fstream>
+using namespace std;
+#include <vector>
+#include <string>
 //Add any includes here
 
 int main() {
-  
+
+  vector<string> countries;
+  vector<vector<int> > debt;
+
+
+  ifstream infile("data.txt");
+  if(!infile) {
+    cout << "data file can not be opened" << endl;
+    return 0;
+  }
+
+
+  string s = "";
+  int temp = 0;
+  while(getline(infile, s)) {
+      cout << s << endl;
+      while(infile >> temp) {
+        cout << temp << endl;
+      }
+  }
+  infile.close();
   
   return 0;
 }
