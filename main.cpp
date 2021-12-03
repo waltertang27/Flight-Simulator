f6bbc6469142c30ca096c3ea9ea06f9c1f273897
Walter Tang <waltert3@illinois.edu>
2021-12-03 16:18:45 -0600

    data parsing test1

--- main.cpp
+++ main.cpp
@@ -3,13 +3,23 @@
 using namespace std;
 #include <vector>
 #include <string>
+#include "graph.h"
+#include <map>
+#include <sstream>
 //Add any includes here
 
 int main() {
 
-  vector<string> countries;
-  vector<vector<int> > debt;
+  map<string, string> airport_names;
 
+  map<string, int> airport_id;
+
+  map<string, vector<string> > routes;
+
+  //vector<string> airlines;
+
+  graph routes();
+  
 
   ifstream infile("data.txt");
   if(!infile) {
@@ -18,14 +28,46 @@ int main() {
   }
 
 
-  string s = "";
-  int temp = 0;
+  
+
+  //stringstream ss(str);
+  string s;
   while(getline(infile, s)) {
-      cout << s << endl;
-      while(infile >> temp) {
-        cout << temp << endl;
-      }
+    string airline;;
+    string airline_id;
+    string source_airport;
+    string source_airport_id;
+    string destination_airport;
+    string destination_airport_id;
+    string codeshare;
+    string stops;
+    string equipment;
+
+    istringstream stream(s);
+    getline(stream, airline, ',');
+
+    getline(stream, airline_id, 's');
+
+    getline(stream, source_airport, ',');
+
+    getline(stream, source_airport_id, ',');
+
+    getline(stream, destination_airport, ',');
+
+    getline(stream, destination_airport_id, ',');
+
+    getline(stream, codeshare, ',');
+
+    getline(stream, stops, ',');
+
+    getline(stream, equipment, '\n');
+
+    routes[source_airport].push_back(destination_airport);
+
   }
+
+
+  
   infile.close();
   
   return 0;
