3d8ce4168af373eb80a78d5c0caa2fe65a3cdf9b
Walter Tang <waltert3@illinois.edu>
2021-12-03 15:48:27 -0600

    small changes

--- graph.h
+++ graph.h
@@ -3,12 +3,17 @@
 #include <vector>
 #include <utility>
 #include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <string>
 using namespace std;
 
 class graph{
     private:
+        unordered_map<string, vector<string> > map;
 
     public:
+
 
 
 };
